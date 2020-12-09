8118540b131c098bc5de533755f00a83a9c7a276
Mateus Santos <m.santos.azeve@gmail.com>
2020-12-09 16:12:34 -0300

    create button to change color

---
+++ js/background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3AA757'}, function() {
+    console.log("The Color is Green.");
+  });
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.instagram.com'},
+      })
+    ],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
